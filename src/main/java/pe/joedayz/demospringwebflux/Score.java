9e5aa9bc54d5febeb8ae05c3d2c304621acd8ab6
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2018-11-10 13:20:12 -0500

    students pojo

---
+++ src/main/java/pe/joedayz/demospringwebflux/Score.java
@@ -0,0 +1,23 @@
+package pe.joedayz.demospringwebflux;
+
+public class Score {
+
+    private double score;
+    private String type;
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
