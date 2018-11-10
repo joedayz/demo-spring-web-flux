756b54dc6726a0c5836dbfcd1ba320fca5a58ff6
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2018-11-10 13:41:31 -0500

    Usando ReactiveDataDriverContextVariable

--- src/main/java/pe/joedayz/demospringwebflux/StudentListController.java
+++ src/main/java/pe/joedayz/demospringwebflux/StudentListController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
 
 @Controller
@@ -19,4 +20,13 @@ public class StudentListController {
         model.addAttribute("students", flux);
         return "students"; // students.html
     }
+
+    @GetMapping("/list-students-reactive")
+    public String listUsersReactive(Model model) {
+        Flux<Student> userFlux = repository.findAll();
+        model.addAttribute("students",
+                new ReactiveDataDriverContextVariable(userFlux, 50));
+        return "students";
+    }
+
 }
